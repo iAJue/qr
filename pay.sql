085150ea1bacfb4a582bd43418b7546769c85132
Lester Chan <yaxisvip@qq.com>
2018-12-19 18:41:37 +0800

    Update pay.sql
    修复导入失败问题

--- pay.sql
+++ pay.sql
@@ -1,23 +1,43 @@
-﻿# Host: localhost  (Version: 5.5.53)
-# Date: 2018-08-29 10:00:59
-# Generator: MySQL-Front 5.3  (Build 4.234)
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.10
+-- https://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: 2018-12-19 18:41:10
+-- 服务器版本： 5.5.62-log
+-- PHP Version: 7.1.24
 
-/*!40101 SET NAMES utf8 */;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-#
-# Structure for table "qr"
-#
 
-DROP TABLE IF EXISTS `qr`;
-CREATE TABLE `qr` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `qr` varchar(128) NOT NULL DEFAULT '',
-  `name` varchar(10) NOT NULL DEFAULT '',
-  `alipay` varchar(255) NOT NULL DEFAULT '',
-  `qq` varchar(255) NOT NULL DEFAULT '',
-  `wechat` varchar(255) NOT NULL DEFAULT '',
-  `time` int(11) NOT NULL DEFAULT '0',
-  `ip` varchar(128) NOT NULL DEFAULT '',
-  `number` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `qrpay`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `qr`
+--
+
+CREATE TABLE IF NOT EXISTS `qr` (
+  `id` int(11) NOT NULL,
+  `qr` int(128) NOT NULL,
+  `name` int(10) NOT NULL,
+  `alipay` int(255) NOT NULL,
+  `qq` int(255) NOT NULL,
+  `wechat` int(255) NOT NULL,
+  `time` int(11) NOT NULL,
+  `ip` int(128) NOT NULL,
+  `number` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
